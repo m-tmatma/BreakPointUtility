89a8d2b1b64dd7419f307f644d8db73cf57dbf75
Masaru Tsuchiyama <m.tmatma@gmail.com>
2017-07-16 16:54:38 +0900

    issue #1 change the command name fix type of the constructor parameter

--- BreakPointUtility/Command.cs
+++ BreakPointUtility/Command.cs
@@ -30,14 +30,14 @@ namespace BreakPointUtility
         /// <summary>
         /// VS Package that provides this command, not null.
         /// </summary>
-        private readonly Package package;
+        private readonly VSPackage package;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Command"/> class.
         /// Adds our command handlers for menu (commands must exist in the command table file)
         /// </summary>
         /// <param name="package">Owner package, not null.</param>
-        private Command(Package package)
+        private Command(VSPackage package)
         {
             if (package == null)
             {
@@ -79,7 +79,7 @@ namespace BreakPointUtility
         /// Initializes the singleton instance of the command.
         /// </summary>
         /// <param name="package">Owner package, not null.</param>
-        public static void Initialize(Package package)
+        public static void Initialize(VSPackage package)
         {
             Instance = new Command(package);
         }
